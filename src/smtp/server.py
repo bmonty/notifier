2f9df4235b79913476b4251c531402bf035d5b27
Benjamin Montgomery <bmontgom@montynet.org>
2022-11-03 17:58:43 -0400

    smtp updates

--- src/smtp/server.py
+++ src/smtp/server.py
@@ -1,2 +1,12 @@
-import asyncio
+from aiosmtpd.controller import Controller
 
+from common.config import SMTPConfig
+from smtp.smtp_handlers import NotifierHandler
+
+
+def start_smtp_server(config: SMTPConfig) -> None:
+    """Starts the SMTP server to handle incoming notifications."""
+    handler = NotifierHandler(config)
+    
+    controller = Controller(handler)
+    controller.start()
